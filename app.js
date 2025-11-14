db952dc084383ebc354bae88bf436184de3be492
Mubeena <mubee.jahir@gmail.com>
2025-11-15 01:53:09 +0530

    change origin

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5174",
+    origin: "*",
     credentials: true,
   })
 );
