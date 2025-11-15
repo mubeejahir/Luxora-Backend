d89996592a205544130c0b385f0d49c908a9222e
Mubeena <mubee.jahir@gmail.com>
2025-11-15 12:14:50 +0530

    refactor: code cleaning

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(
 	cors({
-		origin: "https://luxora-hotel.netlify.app",
+		origin: process.env.ALLOWED_ORIGIN,
 		credentials: true,
 	})
 )
